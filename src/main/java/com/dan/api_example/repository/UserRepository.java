4bc204b2cd1a2281e5d3dc8b3b78cb0ee8ceb832
DongchannN <ldc991104@gmail.com>
2023-05-19 23:33:14 +0900

    @Repository 어노테이션 추가

--- src/main/java/com/dan/api_example/repository/UserRepository.java
+++ src/main/java/com/dan/api_example/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.dan.api_example.repository;
 
 import com.dan.api_example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findById(Long id);
