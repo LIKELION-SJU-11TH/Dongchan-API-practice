7ad16aba1b45a2da49cca9b52b815aa0f9c23c9a
DongchannN <ldc991104@gmail.com>
2023-07-06 14:16:04 +0900

    이미지 업로드, 반환 기능 추가
    createArticleV2,
    viewSingleArticleV2 메서드를 통하여 추가

--- src/main/java/com/dan/api_example/controller/ArticleController.java
+++ src/main/java/com/dan/api_example/controller/ArticleController.java
@@ -3,15 +3,20 @@ package com.dan.api_example.controller;
 import com.dan.api_example.common.exception.BaseException;
 import com.dan.api_example.common.response.BaseResponse;
 import com.dan.api_example.model.article.GetArticleRes;
+import com.dan.api_example.model.article.GetArticleResWithImg;
 import com.dan.api_example.model.article.PostArticleReq;
 import com.dan.api_example.service.ArticleService;
 import com.dan.api_example.util.JwtUtils;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 public class ArticleController {
@@ -31,7 +36,7 @@ public class ArticleController {
         Long userIdx = jwtUtils.getUserIdV2();
 
         try {
-            articleService.saveArticle(userIdx, postBoardReq);
+            articleService.createArticle(userIdx, postBoardReq);
             return new BaseResponse<>("게시물을 등록하였습니다.");
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
@@ -90,6 +95,45 @@ public class ArticleController {
         try {
             GetArticleRes articleRes = articleService.viewSingleArticle(boardIdx);
             return new BaseResponse<>(articleRes);
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    /**
+     * 이미지 업로드 버전
+     *
+     * @return
+     */
+    @PostMapping(value = "/board/addV2", consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public BaseResponse<String> createBoardV2(@RequestPart PostArticleReq postBoardReq, @RequestPart(value = "files", required = false) List<MultipartFile> files) {
+        log.info("MULTIPART DATA : {}", files.get(0));
+
+        Long userIdx = jwtUtils.getUserIdV2();
+        if (files == null || files.isEmpty()) {
+            try {
+                articleService.createArticle(userIdx, postBoardReq);
+                return new BaseResponse<>("게시물 업로드에 성공하였습니다.(NO IMG)");
+            } catch (BaseException e) {
+                return new BaseResponse<>(e.getStatus());
+            }
+        }
+
+        try {
+            articleService.createArticleV2(userIdx, postBoardReq, files);
+            return new BaseResponse<>("게시물 업로드에 성공하였습니다.");
+        } catch (BaseException e) {
+            return new BaseResponse<>(e.getStatus());
+        }
+    }
+
+    @GetMapping("/boardV2")
+    public BaseResponse<GetArticleResWithImg> viewSingleBoardV2(@RequestParam("boardId") Long boardIdx) {
+        jwtUtils.getUserIdV2();
+
+        try {
+            GetArticleResWithImg getArticleResWithImg = articleService.viewSingleArticleV2(boardIdx);
+            return new BaseResponse<>(getArticleResWithImg);
         } catch (BaseException e) {
             return new BaseResponse<>(e.getStatus());
         }
--- src/main/java/com/dan/api_example/service/ArticleService.java
+++ src/main/java/com/dan/api_example/service/ArticleService.java
@@ -2,18 +2,38 @@ package com.dan.api_example.service;
 
 import com.dan.api_example.common.exception.BaseException;
 import com.dan.api_example.entity.Article;
+import com.dan.api_example.entity.Image;
 import com.dan.api_example.entity.User;
 import com.dan.api_example.model.article.GetArticleRes;
+import com.dan.api_example.model.article.GetArticleResWithImg;
 import com.dan.api_example.model.article.PostArticleReq;
+import com.dan.api_example.model.image.GetImageRes;
 import com.dan.api_example.model.user.GetUserRes;
 import com.dan.api_example.repository.ArticleRepository;
+import com.dan.api_example.repository.ImageRepository;
 import com.dan.api_example.repository.UserRepository;
+import com.dan.api_example.util.ImageUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,9 +46,10 @@ import static com.dan.api_example.common.response.BaseResponseStatus.*;
 public class ArticleService {
     private final ArticleRepository articleRepository;
     private final UserRepository userRepository;
+    private final ImageUtil imageUtil;
+    private final ImageRepository imageRepository;
 
-
-    public void saveArticle(Long userIdx, PostArticleReq postArticleReq) {
+    public Article createArticle(Long userIdx, PostArticleReq postArticleReq) {
         User user = userRepository.findByIdAndState(userIdx, ACTIVE)
                 .orElseThrow(() -> new BaseException(NON_EXIST_USER));
         log.info("USER 이름 : {}", user.getName());
@@ -45,7 +66,7 @@ public class ArticleService {
         log.info("게시글 빌드 성공 : {}", article.getTitle());
 
         try {
-            articleRepository.save(article);
+            return articleRepository.save(article);
         } catch (Exception e) {
             throw new BaseException(DATABASE_INSERT_ERROR);
         }
@@ -114,5 +135,64 @@ public class ArticleService {
                 .build();
 
         return getArticleRes;
+    }
+
+    public void createArticleV2(Long userIdx, PostArticleReq postArticleReq, List<MultipartFile> files) {
+        Article savedArticle = createArticle(userIdx, postArticleReq);
+        List<String> imgNameList;
+        try {
+            imgNameList = imageUtil.uploadImage(files);
+        } catch (Exception e) {
+            log.info("Exception e : {}", e.getMessage());
+            throw new BaseException(EMPTY_MULTIPARTFILE);
+        }
+
+        try {
+            for (String imgName : imgNameList) {
+                Image imgFile = Image.builder()
+                        .url(imgName)
+                        .article(savedArticle).build();
+                imageRepository.save(imgFile);
+            }
+        } catch (Exception e) {
+            throw new BaseException(DATABASE_INSERT_ERROR);
+        }
+    }
+
+    public GetArticleResWithImg viewSingleArticleV2(Long boardId) {
+        Article article = articleRepository.findByIdAndState(boardId, ACTIVE)
+                .orElseThrow(() -> new BaseException(NON_EXIST_ARTICLE));
+
+        List<GetImageRes> data = new ArrayList<>();
+        if (article.getImages() != null) {
+            List<Image> files = article.getImages();
+            for (Image file : files) {
+                Long fileId = file.getId();
+                Image image = imageRepository.findById(fileId).orElseThrow(() -> new BaseException(NON_EXIST_IMAGE));
+
+                String stringUrl = image.getUrl();
+                Path imagePath = Paths.get(stringUrl);
+
+                byte[] imgData = null;
+                try {
+                    imgData = Files.readAllBytes(imagePath);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                data.add(new GetImageRes(imgData));
+                log.info("DATA : {}", data.get(0).getData().length);
+            }
+        }
+
+        GetArticleResWithImg getArticleResWithImg = GetArticleResWithImg.builder()
+                .writer(new GetUserRes(article.getUser()))
+                .title(article.getTitle())
+                .content(article.getContent())
+                .createdAt(article.getCreatedAt())
+                .imageList(data)
+                .build();
+
+        return getArticleResWithImg;
     }
 }
